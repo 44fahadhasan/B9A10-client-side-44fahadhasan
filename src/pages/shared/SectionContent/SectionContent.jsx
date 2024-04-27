f22a763dae1b715b3058661ea1d238accc94c616
Fahad Hasan <44fahadhasan@gmail.com>
2024-04-27 12:16:24 +0600

    added register page without functionality

---
+++ src/pages/shared/SectionContent/SectionContent.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+const SectionContent = ({ leftTitle, rightTitle, content }) => {
+  return (
+    <div className="text-center space-y-3">
+      <h2 className="dmsans text-2xl sm:text-4xl font-bold text-primary-content">
+        {leftTitle} <span className="text-primary">{rightTitle}</span>
+      </h2>
+      <p className="poppins text-[15px] text-secondary-content font-light">
+        {content}
+      </p>
+    </div>
+  );
+};
+
+SectionContent.propTypes = {
+  leftTitle: PropTypes.string.isRequired,
+  rightTitle: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+};
+
+export default SectionContent;
--- src/routes/PubliceRoute.jsx
+++ src/routes/PubliceRoute.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import LandingPage from "../pages/LandingPage/LandingPage";
+import RegisterPage from "../pages/RegisterPage/RegisterPage";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <LandingPage />,
+  },
+  {
+    path: "register",
+    element: <RegisterPage />,
   },
 ]);
 export default routes;
